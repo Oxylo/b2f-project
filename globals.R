771eca9b7ae61210e854fbe2418c31f379ba83b4
pieter <info@oxylo.com>
2017-04-19 16:55:07 +0200

    initial commit

---
+++ globals.R
@@ -0,0 +1,8 @@
+# ------------------------
+# globals.R
+# -----------------------
+# Contains global varaibles (constants)
+
+# credential to access AWS database
+USERNAME <- 'pieter'
+PASSWORD <- 'cerveza$1'
